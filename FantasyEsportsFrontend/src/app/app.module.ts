7af343c1ced6e00fe0fafd0ecb986d5d9c0985fb
unknown <rillee.johansson@gmail.com>
2018-11-01 20:28:17 +0100

    Added angular router and some routes

--- FantasyEsportsFrontend/src/app/app.module.ts
+++ FantasyEsportsFrontend/src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { OverviewComponent } from '../pages/overview/overview.component';
+import { StartComponent } from '../pages/start/start.component';
+import { NotFoundComponent } from '../errorpages/not-found.component';
 
 import { AppComponent } from './app.component';
 
+const appRoutes: Routes = [
+  { path: 'start', component: StartComponent },
+  { path: 'overview', component: OverviewComponent },
+  { path: '**', component: NotFoundComponent }
+];
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
+    OverviewComponent,
+    StartComponent
   ],
   imports: [
     NgbModule,
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ FantasyEsportsFrontend/src/pages/overview/overview.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'overview-page',
+  templateUrl: './overview.component.html',
+  styleUrls: ['./overview.component.css']
+})
+export class OverviewComponent {
+}
