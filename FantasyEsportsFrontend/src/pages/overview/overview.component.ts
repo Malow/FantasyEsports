8a29b0b26177c683270de9bb6544fe36ae1618d7
unknown <rillee.johansson@gmail.com>
2018-11-04 20:15:10 +0100

    Remove files that wasn't removed

--- FantasyEsportsFrontend/src/pages/overview/overview.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'overview-page',
-  templateUrl: './overview.component.html',
-  styleUrls: ['./overview.component.css']
-})
-export class OverviewComponent {
-}
